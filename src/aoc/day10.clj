81caccafe062ae5471a9a0161f6347c348bed6b6
Benny Thomas <jan.thomas@speilbildet.com>
2018-01-03 09:06:06 +0100

    better understandig of day 10 part 2 but not quite there

--- src/aoc/day10.clj
+++ src/aoc/day10.clj
@@ -3,11 +3,23 @@
 
 (def puzzle10 (take 256 (iterate inc 0)))
 
-(def inputs [14 58 0 116 179 16 1 104 2 254 167 86 255 55 122 244])
+(def inputs '(14 58 0 116 179 16 1 104 2 254 167 86 255 55 122 244))
 (def byteinputs "14,58,0,116,179,16,1,104,2,254,167,86,255,55,122,244")
-(def suffix [17 31 73 47 23])
+(def suffix '(17 31 73 47 23))
 
-(def ascii-inputs (flatten (repeat 64 (concat (map #(int %) byteinputs) suffix))))
+(defn create-lengths
+  ""
+  [input]
+  (flatten
+    ;; (repeat 64
+      (concat
+        (map #(int %) input) suffix)))
+    ;; )
+
+(def test-input (create-lengths ""))
+(def test-input1 (create-lengths "1,2,3"))
+(def test-input2 (create-lengths "1,2,4"))
+(def ascii-inputs (create-lengths byteinputs))
 
 (defn do-hash
   ""
@@ -23,20 +35,31 @@
           (concat (drop (- len overflow) reversed) (drop overflow (take pos stream)) (take (- len overflow) reversed))
           )))))
 
-(defn elves-hash
+(defn elves-hash-repeated
   ""
-  [stream inputs]
-  (loop [re-stream stream re-inputs inputs pos 0 skip 0]
+  [stream inputs pos skip]
+  (loop [re-stream stream re-inputs inputs pos pos skip skip]
     (if (empty? re-inputs)
-      re-stream
-      (let [len (first re-inputs) position (if (> pos (count re-stream)) (- pos (count re-stream)) pos)]
+      {:stream re-stream :pos pos :skip skip}
+      (let [len (first re-inputs)]
         ;; do-something with the stream
         ;; (println "- " position " : " len " : " skip)
-        (let [result (do-hash re-stream position len)]
+        (let [result (do-hash re-stream pos len)]
           ;; (println ":: " result)
           ;(println (count re-inputs) ": " position " + " len " -> " (count (distinct result)))
-          (recur result (rest re-inputs) (+ position len skip) (inc skip))
-        )))))
+          (recur result (rest re-inputs) (mod (+ pos len skip) (count stream)) (inc skip)))
+        ))))
+
+(defn elves-hash
+  ""
+  [stream inputs & [n]]
+  (loop [pos 0 skip 0 result nil n (if (nil? n) 1 n)]
+    (if (= n 0)
+      result
+      (let [res (elves-hash-repeated stream inputs pos skip)]
+        (println res)
+        (recur (get res :pos) (get res :skip) (get res :stream) (dec n))
+        ))))
 
 (defn dense-hash
   ""
@@ -49,6 +72,6 @@
 (defn hex-notation
   ""
   [stream]
-  (map #(format "%x" %) (take 16 stream)))
+  (apply str (map #(format "%x" %) (take 16 stream))))
 
 (def puzzle10-part1 (apply * (take 2 (elves-hash puzzle10 inputs))))
